7649929567c98ff352cba45026cbfdfb7679a9e6
aatomu <aatomu21263+git@gmail.com>
2025-01-22 13:47:12 +0900

    ⚡️ protocol verを可変に

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,77 @@ interface readResult<T> {
 	value: T;
 }
 
+const protocolVersions: { [mcVersion: string]: number } = {
+	'1.21.4': 769,
+	'1.21.3': 768,
+	'1.21.2': 768,
+	'1.21.1': 767,
+	'1.21': 767,
+	'1.20.6': 766,
+	'1.20.5': 766,
+	'1.20.4': 765,
+	'1.20.3': 765,
+	'1.20.2': 764,
+	'1.20.1': 763,
+	'1.20': 763,
+	'1.19.4': 762,
+	'1.19.3': 761,
+	'1.19.2': 760,
+	'1.19.1': 760,
+	'1.19': 759,
+	'1.18.2': 758,
+	'1.18.1': 757,
+	'1.18': 757,
+	'1.17.1': 756,
+	'1.17': 755,
+	'1.16.5': 754,
+	'1.16.4': 754,
+	'1.16.3': 753,
+	'1.16.2': 751,
+	'1.16.1': 736,
+	'1.16': 735,
+	'1.15.2': 578,
+	'1.15.1': 575,
+	'1.15': 573,
+	'1.13.2': 404,
+	'1.13.1': 401,
+	'1.13': 393,
+	'1.12.2': 340,
+	'1.12.1': 338,
+	'1.12': 335,
+	'1.11.2': 316,
+	'1.11.1': 316,
+	'1.11': 315,
+	'1.10.2': 210,
+	'1.10.1': 210,
+	'1.10': 210,
+	'1.9.4': 110,
+	'1.9.3': 110,
+	'1.9.2': 109,
+	'1.9.1': 108,
+	'1.9': 107,
+	'1.8.9': 47,
+	'1.8.8': 47,
+	'1.8.7': 47,
+	'1.8.6': 47,
+	'1.8.5': 47,
+	'1.8.4': 47,
+	'1.8.3': 47,
+	'1.8.2': 47,
+	'1.8.1': 47,
+	'1.8': 47,
+	'1.7.10': 5,
+	'1.7.9': 5,
+	'1.7.8': 5,
+	'1.7.7': 5,
+	'1.7.6': 5,
+	'1.7.5': 4,
+	'1.7.4': 4,
+	'1.7.3': 4,
+	'1.7.2': 4,
+	'1.7.1': 3,
+	'1.7': 3,
+};
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
@@ -60,6 +131,15 @@ export default {
 			} as result);
 		}
 		const port = params.get('port') || '25565';
+		const protocol = (() => {
+			const version = params.get('version');
+			if (!version) {
+				const latestVersion = Object.entries(protocolVersions)[0]
+				return latestVersion[1];
+			}
+			return protocolVersions[version];
+		})();
+		console.log(protocol);
 
 		// DNS resolve
 		const target = await DNSresolve(address, Number(port));
@@ -105,7 +185,7 @@ export default {
 
 			// server status request
 			// 1.sent handshake
-			const handshake = handshakePacket(target.hostname, target.port);
+			const handshake = handshakePacket(protocol, address, target.port);
 			console.log(`sent handshake: ${handshake}`);
 			await writer.write(handshake);
 			await sleep(50);
@@ -283,10 +363,10 @@ async function readVarInt(reader: ReadableStreamBYOBReader): Promise<readResult<
 	};
 }
 
-function handshakePacket(address: string, port: number): Uint8Array {
+function handshakePacket(protocol: number, address: string, port: number): Uint8Array {
 	let data = new Uint8Array();
 
-	const protocolVer = writeVarInt(3);
+	const protocolVer = writeVarInt(protocol);
 	data = concatUint8Array(data, protocolVer);
 	const addressBuf = new TextEncoder().encode(address);
 	const addressLen = writeVarInt(addressBuf.length);
